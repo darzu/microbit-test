0fff7f9f7f5b8b3f1b38ecd0a1eee9151cd75dad
Daryl Zuniga <Daryl.Zuniga@gmail.com>
2020-04-17 11:33:37 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,5 +13,7 @@ pins.onPulsed(DigitalPin.P0, PulseValue.High, function () {
 	
 })
 basic.forever(function () {
-	
+    basic.showNumber(0)
+    basic.showNumber(Math.randomRange(1, 6))
+    basic.showNumber(3)
 })
