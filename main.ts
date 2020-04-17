757366720317a26aee9de994596ecb594d66cc79
Daryl Zuniga <Daryl.Zuniga@gmail.com>
2020-04-17 11:32:35 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+radio.onReceivedValue(function (name, value) {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    led.plot(0, 0)
+    led.plot(0, 0)
+    led.plot(0, 0)
+    led.plot(0, 0)
+    led.plot(0, 0)
+    led.plot(0, 0)
+})
+pins.onPulsed(DigitalPin.P0, PulseValue.High, function () {
+	
+})
+basic.forever(function () {
+	
+})
